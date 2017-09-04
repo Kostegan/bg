9a67f92dd698831895e3d278e7a76fe0aefff3b0
Kostegan <churilovkonstantin@gmail.com>
2017-09-05 00:59:14 +0300

    Added ui class and create Main class

---
+++ src/main/java/com/bg/Main.java
@@ -0,0 +1,11 @@
+package com.bg;
+
+public class Main {
+    static String[] s = new String[1];
+
+    public static void main(String[] args) {
+
+        s[0] = "asd";
+        System.out.println(s[0]);
+    }
+}
---
+++ src/main/java/com/bg/ui/IUI.java
@@ -0,0 +1,4 @@
+package com.bg.ui;
+
+public class IUI {
+}
